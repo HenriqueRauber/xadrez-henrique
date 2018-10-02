8c11645b75c9cf67b9bcbd9eeb76f92201d3a22f
Henrique Rauber <henriquerauber@gmail.com>
2018-10-02 12:45:52 -0300

    Comecando a implementar o metodo que imprime o tabuleiro

---
+++ xadrez-henrique/Tela.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_henrique
+{
+    class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                { 
+                    Console.Write($"{tab.peca(i, j)?.ToString() ?? "-"} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

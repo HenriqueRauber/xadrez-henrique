5abe5fb93937b4dcccc6f5596c8f0ff9b78b1f3f
Henrique Rauber <henriquerauber@gmail.com>
2018-10-03 12:58:47 -0300

    Melhorando a implementacao do metodo imprimir tabuleiro

--- xadrez-henrique/Program.cs
+++ xadrez-henrique/Program.cs
@@ -5,35 +5,28 @@ using xadrez;
 namespace xadrez_henrique
 {
     class Program
-    {
-
+    { 
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.Write(pos);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(1, 4));
 
-            Console.WriteLine(pos.toPosicao());
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 7));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(6, 3));
 
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.Write(e.Message);
+            }
             Console.ReadLine();
         }
-
-        //static void Main(string[] args)
-        //{
-        //    try
-        //    {
-        //        Tabuleiro tab = new Tabuleiro(8, 8);
-
-        //        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-        //        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
-        //        tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(1, 4));
-
-        //        Tela.imprimirTabuleiro(tab);
-        //    }catch(TabuleiroException e)
-        //    {
-        //        Console.Write(e.Message);
-        //    }
-        //    Console.ReadLine();
-        //}
     }
 }
